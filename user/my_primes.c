8de142966ba08e6ed9a37b321cacf83e92be5faf
matengyu888 <2064143983@qq.com>
2025-03-14 14:53:24 +0800

    prime7

---
+++ user/my_primes.c
@@ -0,0 +1,69 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+void runpross(int readfd)
+{
+    int f_fork=0;
+    int prime=0;
+    int pipes[2];
+    int num=0;
+    while(1)
+    {
+        int read_bytes = read(readfd, &num, 4);
+
+        // left neighbor has no more number to provide
+        if (read_bytes == 0) {
+          close(readfd);
+          if (f_fork) {
+            // tell my children I have no more number to offer
+            close(pipes[1]);
+            // wait my child termination
+            int child_pid;
+            wait(&child_pid);
+          }
+          exit(0);
+        }
+        if(prime==0)
+        {
+            prime=num;
+            printf("Prime: %d\n", prime);
+        }
+
+        if(num%prime!=0)
+        {
+            if(!f_fork)
+            {
+                pipe(pipes);
+                f_fork=1;
+                int ret=fork();
+                if(ret==0)
+                {
+                    close(readfd);
+                    close(pipes[1]);
+                    runpross(pipes[0]);
+                }
+                else
+                {
+                    close(pipes[0]);
+                }
+            }
+            write(pipes[1], &num, sizeof(num));
+
+        }
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    int p[2];
+    pipe(p);
+    for(int i=2;i<36;i++)
+    {
+        write(p[1], &i, sizeof(i));
+    }
+    close(p[1]);
+    runpross(p[0]);
+    exit(0);
+  }
--- user/primes.c
+++ user/primes.c
@@ -1,69 +1,67 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
+/*
+ * Run as a prime-number processor
+ * the listenfd is from your left neighbor
+ */
+void runprocess(int listenfd) {
+  int my_num = 0;
+  int forked = 0;
+  int passed_num = 0;
+  int pipes[2];
+  while (1) {
+    int read_bytes = read(listenfd, &passed_num, 4);
 
-void runpross(int readfd)
-{
-    int f_fork=0;
-    int prime=0;
-    int pipes[2];
-    int num=0;
-    while(1)
-    {
-        int read_bytes = read(readfd, &num, 4);
-
-        // left neighbor has no more number to provide
-        if (read_bytes == 0) {
-          close(readfd);
-          if (f_fork) {
-            // tell my children I have no more number to offer
-            close(pipes[1]);
-            // wait my child termination
-            int child_pid;
-            wait(&child_pid);
-          }
-          exit(0);
-        }
-        if(prime==0)
-        {
-            prime=num;
-            printf("Prime: %d\n", prime);
-        }
-
-        if(num%prime!=0)
-        {
-            if(!f_fork)
-            {
-                pipe(pipes);
-                f_fork=1;
-                int ret=fork();
-                if(ret==0)
-                {
-                    close(readfd);
-                    close(pipes[1]);
-                    runpross(pipes[0]);
-                }
-                else
-                {
-                    close(pipes[0]);
-                }
-            }
-            write(pipes[1], &num, sizeof(num));
-
-        }
+    // left neighbor has no more number to provide
+    if (read_bytes == 0) {
+      close(listenfd);
+      if (forked) {
+        // tell my children I have no more number to offer
+        close(pipes[1]);
+        // wait my child termination
+        int child_pid;
+        wait(&child_pid);
+      }
+      exit(0);
     }
 
+    // if my initial read
+    if (my_num == 0) {
+      my_num = passed_num;
+      printf("prime %d\n", my_num);
+    }
+
+    // not my prime multiple, pass along
+    if (passed_num % my_num != 0) {
+      if (!forked) {
+        pipe(pipes);
+        forked = 1;
+        int ret = fork();
+        if (ret == 0) {
+          // i am the child
+          close(pipes[1]);
+          close(listenfd);
+          runprocess(pipes[0]);
+        } else {
+          // i am the parent
+          close(pipes[0]);
+        }
+      }
+
+      // pass the number to right neighbor
+      write(pipes[1], &passed_num, 4);
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
-    int p[2];
-    pipe(p);
-    for(int i=2;i<36;i++)
-    {
-        write(p[1], &i, sizeof(i));
-    }
-    close(p[1]);
-    runpross(p[0]);
-    exit(0);
+  int pipes[2];
+  pipe(pipes);
+  for (int i = 2; i <= 35; i++) {
+    write(pipes[1], &i, 4);
   }
+  close(pipes[1]);
+  runprocess(pipes[0]);
+  exit(0);
+}
