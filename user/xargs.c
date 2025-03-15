451aad764ba44c91d13db23b0b7c0eff688d8563
matengyu888 <2064143983@qq.com>
2025-03-15 15:33:25 +0800

    xargs

--- user/xargs.c
+++ user/xargs.c
@@ -12,15 +12,39 @@ int main(int argc, char *argv[])
   char buf[MAXARG];
   read(0, buf, MAXARG);
   printf("buf: %s\n", buf);
-  // char *p = buf;
 
-  // int xargc = 0;
-  // char *xargv[MAXARG];
-  // for(int i = 1; i < MAXARG; i++)
-  // {
-  //   xargv[xargc] = argv[i];
-  //   xargc++;
-  // }
+  int xargc = 0;
+  char *xargv[MAXARG];
+  for(int i = 1; i < argc; i++)
+  {
+    xargv[xargc] = argv[i];
+    xargc++;
+  }
+  char *p = buf;
+  for(int i=0;i<MAXARG;i++)
+  {
+    if(buf[i]=='\n')
+    {
+      int pid=fork();
+      if(pid>0)
+      {
+        p=&buf[i+1];
+        wait(0);
+      }
+      else if(pid==0)
+      {
+        xargv[xargc]=p;
+        xargc++;
+        xargv[xargc]=0;
+        xargc++;
+        exec(xargv[0],xargv);
+        exit(1);
+      }
+
+    }
+  }
+
+  wait(0);
   exit(0);
 }
 
